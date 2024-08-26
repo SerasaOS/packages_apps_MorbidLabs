1a6fb861ce9bd1174882e5f71310e073406f7c72
Kneba <abenkenary3@gmail.com>
2024-08-26 13:29:39 +0000

    fixup! font & header

--- src/com/morbid/labs/fragments/themes/fonts/FontArrayAdapter.java
+++ src/com/morbid/labs/fragments/themes/fonts/FontArrayAdapter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.morbid.filabseld.fragments.themes.fonts;
+package com.morbid.labs.fragments.themes.fonts;
 
 import android.content.Context;
 import android.graphics.Typeface;
